b8e201ebeb7c93ad7f28bfe89b096a12de570e98
VAVILON <ivan.pechyonov@mail.ru>
2020-10-15 07:23:03 +0400

    Создание прототипов функций работы с данными. Реализация функций чтения из txt файла и xml файла. Готовые комментарии

--- CorrectWay/CorrectWay/function.cpp
+++ CorrectWay/CorrectWay/function.cpp
@@ -1,1 +1,35 @@
 #include "function.h"
+
+void readFileContent (const std::string& filePath, std::string& fileContent, std::vector<ErrorInfo>& errorsInfo){
+
+    std::ifstream file(filePath);                 // Попробовать открыть файл для чтения
+
+    if(file.is_open()){                             // Если файл доступен
+        fileContent.clear();                       // Очистить место для загрузки содержимого файла
+
+        std::string temp = "";                    // Содержимое строки файла
+        while(std::getline(file, temp)){       // Пока не считали все строки файла
+            fileContent += temp + "\n";      // Считать очередную строку содержимого файла
+        }
+    } else {errorsInfo.push_back(ErrorInfo(ERR_FILE, {QString::fromStdString(filePath)}));}        // Иначе считать, что возникла ошибка при чтении файла
+}
+
+bool readXMLTreeFromFile(const std::string& xmlFilePath, tinyxml2::XMLDocument& xmlDoc, std::vector<ErrorInfo>& errorsInfo){
+    bool isFileNotEmpty = true;                    // Признак наличия содержимого в файле
+    std::string xmlFileContent;                       // Содержимое XML файла
+    std::vector<ErrorInfo> currErrorsInfo;    // Набор ошибок чтения XML дерева из файла
+
+    readFileContent(xmlFilePath, xmlFileContent, currErrorsInfo);  // Чтение содержимого XML файла
+    isFileNotEmpty = (xmlFileContent != "");
+
+    if(!xmlFileContent.empty() && currErrorsInfo.empty()){ // Если не было ошибок при чтении XML файла и XML файл не пустой
+        xmlDoc.Parse(xmlFileContent.c_str());                      // Получение XML дерева из содержимого XML файла
+
+        if(xmlDoc.Error() ){// Считать, что возникла ошибка, если были ошибки при получении XML дерева
+         currErrorsInfo.push_back( ErrorInfo( ERROR_FILE_XML_OPEN, { { QString::fromStdString(xmlFilePath) }, { QString::fromStdString(xmlDoc.ErrorStr()) } }  ) );
+    }
+        errorsInfo.insert(errorsInfo.end(), currErrorsInfo.begin(), currErrorsInfo.end());      // Добавить полученные ошибки в общий набор ошибок работы программы
+    }
+    return isFileNotEmpty;
+}
+
--- CorrectWay/CorrectWay/function.h
+++ CorrectWay/CorrectWay/function.h
@@ -1,1 +1,63 @@
 #include "dataStructure.h"
+
+/*!
+ * \brief Функция чтения содержимого файла
+ * \param [in] filePath  - путь к файлу для чтения
+ * \param [out] fileContent  - полученное содержимое файла
+ * \param [out] errorsInfo - набор ошибок чтения файла
+ */
+void readFileContent (const std::string& filePath, std::string& fileContent, std::vector<ErrorInfo>&errorsInfo);
+
+/*!
+ * \brief Функция чтения XML дерева из файла
+ * \param [in] xmlFilePath - путь к файлу XML для чтения
+ * \param [out] xmlDoc - выходное XML дерево
+ * \param [out] errorsInfo - набор ошибок чтения XML дерева из файла
+ * \return Признак наличия содержимого в XML файле
+ */
+bool readXMLTreeFromFile(const std::string& xmlFilePath, tinyxml2::XMLDocument& xmlDoc,  std::vector<ErrorInfo>&errorsInfo);
+
+/*!
+ * \brief Функция выделения информации о переменных
+ * \param [in] docXML - XML дерево с описанием необходимых данных о переменных, функциях, пользовательских типах данных
+ * \param [out] errorsInfo - набор описаний ошибок при чтении файла
+ * \return Набор информации о переменных
+ */
+const std::vector<VariableInfo*> readVarsInfoFromXML(const tinyxml2::XMLDocument& docXML,  std::vector<ErrorInfo>&errorsInfo);
+
+/*!
+ * \brief Функция выделения информации о функциях
+ * \param [in] docXML - XML дерево с описанием необходимых данных о переменных, функциях, пользовательских типах данных
+ * \param [out] errorsInfo - набор описаний ошибок при чтении файла
+ * \return Набор информации о функциях
+ */
+const std::vector<FunctionInfo*> readFuncInfoFromXML(const tinyxml2::XMLDocument& docXML,  std::vector<ErrorInfo>&errorsInfo);
+
+/*!
+ * \brief Функция выделения информации о пользовательских типах данных
+ * \param [in] docXML - XML дерево с описанием необходимых данных о переменных, функциях, пользовательских типах данных
+ * \param [out] errorsInfo - набор описаний ошибок при чтении файла
+ * \return Набор информации о пользовательских типах данных
+ */
+const std::vector <CustomDataInfo*> readCustomDataInfoFromXML(const tinyxml2::XMLDocument& docXML,  std::vector<ErrorInfo>&errorsInfo);
+
+/*!
+* \brief Функция проверка корректности типов данных
+* \param [in] str - строковое представление типа данных
+* \return Может ли строка являться корректным типом данных
+ */
+bool isCorrectCppType(const QString &str);
+
+/*!
+* \brief Проверка может ли строка являться именем переменной
+* \param [in] str - строка с возможным именем переменной
+* \return Может ли строка являться именем переменной
+ */
+bool checkCorrectVarName(const QString &str);
+
+/*!
+* \brief Функция для определения является ли поданная строка ключевым словом
+* \param str - строка с предпологаемым ключевым словом
+* \return Может ли строка являться  ключевым словом
+*/
+bool isCppKeyword(const QString &str);
